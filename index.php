563b3c79d004851335597aa8b6cc7a9e44ee14bd
LEYKAZA <139692344+zephleroy@users.noreply.github.com>
2025-10-07 23:10:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?PHP
 
+/**
+	* Check for database configuration file.
+	* If it exists, proceed to login page.
+	* If it doesn't, call the setup page.
+	*/
+	
+	if(file_exists('config/config.php')) header('Location: login.php');
+	
+	else header('Location: setup.php');
+
+?>
